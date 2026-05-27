8b3d188c832e017cf6840af08e0078633e00dee6
sam01-love <akins4just@gmail.com>
2026-05-27 10:08:06 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,7 +20,6 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
