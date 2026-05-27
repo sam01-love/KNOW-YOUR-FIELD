8ffdc743cec3c7448e7890556425e0f07df5fa59
sam01-love <akins4just@gmail.com>
2026-05-27 10:54:34 +0100

    Refactor Firebase initialization and authentication logic

--- firebase.js
+++ firebase.js
@@ -22,6 +22,20 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+
 
 import {
     createUserWithEmailAndPassword
@@ -46,7 +60,7 @@ signInWithEmailAndPassword(auth, email, password)
 
 // CREATE ACCOUNT
 const signupBtn =
-document.getElementById("signup-btn");
+document.getElementByClassName("btn-submit");
 
 signupBtn.addEventListener("click", () => {
 
@@ -81,7 +95,7 @@ signupBtn.addEventListener("click", () => {
 
 // LOGIN
 const loginBtn =
-document.getElementById("login-btn");
+document.getElementByClassName("btn-submit");
 
 loginBtn.addEventListener("click", () => {
 
