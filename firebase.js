0de64c74ed72f5d486a3bab8c7eb801ee2490aeb
sam01-love <akins4just@gmail.com>
2026-05-27 10:33:41 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,26 @@
 // Import from a CDN like unpkg or esm.sh
+
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIagZ5KfOURA9jyKVwPP4XopYpayQbmSM",
+    authDomain: "know-your-field.firebaseapp.com",
+    projectId: "know-your-field",
+    storageBucket: "know-your-field.firebasestorage.app",
+    messagingSenderId: "149807278945",
+    appId: "1:149807278945:web:3da1ab085900b3bbf4b320",
+    measurementId: "G-5CBKDCV2ND"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";;
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase/firestore.js"
